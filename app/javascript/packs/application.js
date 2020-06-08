40c7c72ae8ec45e4afc94a534c835dd0454125e1
Alessandro Stein <alessandro@alessandrostein.com>
2020-06-08 19:16:11 -0300

    Install StimulusJs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,18 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Turbolinks from "turbolinks";
+Turbolinks.start();
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs";
+Rails.start();
 
+// Utility function to require all files in a directory...
+function requireAll(r) {
+  // console.log(r.keys());
+  r.keys().forEach(r);
+}
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("../controllers", true, /.js$/); // eslint-disable-line no-undef
+application.load(definitionsFromContext(context));
